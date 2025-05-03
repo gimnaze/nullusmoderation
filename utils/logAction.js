6f99015cc3f7e559bb3b41cf64241fb109643053
Gimnaze <152474486+xenodevlabs@users.noreply.github.com>
2025-05-03 22:54:03 +1000

    Revert "Uploaded Files"
    This reverts commit 41e0e5c6bb8fd8154535aadf6b13e28feed4c8dc.

--- utils/logAction.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function logAction(guild, embed, config) {
-    const channel = guild.channels.cache.get(config.modLogChannelId);
-    if (channel) channel.send({ embeds: [embed] });
-  };  
